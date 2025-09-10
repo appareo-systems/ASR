d33d73f6db15c15ac63290fa14c6c5f2b0982029
DJ Schwartz <dj.schwartz@gmail.com>
2025-09-10 08:42:28 -0700

    Fix typo in package.swift.

--- Package.swift
+++ Package.swift
@@ -13,12 +13,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ASR",
-            url: "https://github.com/appareo-systems/ASR/releases/download/0.0.2/ASR.xcframework.zip",
+            url: "https://github.com/appareo-systems/ASR/releases/download/0.0.3/ASR.xcframework.zip",
             checksum: "3923651d77cad8d7d53dbdf3195bbb632b50ff4833413635e96f917ed439b6a0"
         ),
         .binaryTarget(
             name: "eesen_carthage",
-            url: "https://github.com/appareo-systems/ASR/releases/download/0.0.2/eesen_carthage.xcframework.zip",
+            url: "https://github.com/appareo-systems/ASR/releases/download/0.0.3/eesen_carthage.xcframework.zip",
             checksum: "702bf9418006216500bad674e4c80a675c950c61e05572db0e53314be82ab6e3"
         )
     ]
