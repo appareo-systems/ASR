f7908c8e337c8a0f742b0e6f9832d07b0ecfdd0c
Bill Burgess <bburgess@appareo.com>
2026-09-14 10:29:54 -0500

    New version to fix cache problem

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ASR",
-            url: "https://github.com/appareo-systems/ASR/releases/download/0.0.4/ASR.xcframework.zip",
-            checksum: "4d740627a4eebf14b02790ce9b4893698ef976f269b5e17bb22a41441f322374"
+            url: "https://github.com/appareo-systems/ASR/releases/download/0.0.5/ASR.xcframework.zip",
+            checksum: "0bd34b0de5c1b8a722d94492bfe877c6bcd5a5caf867cfc27ed30e89eb49fd1f"
         ),
         .binaryTarget(
             name: "eesen_carthage",
-            url: "https://github.com/appareo-systems/ASR/releases/download/0.0.4/eesen_carthage.xcframework.zip",
-            checksum: "1a2422cd69cecbea07291ba3aefee42e367efe25795a16192740bd6ba64d688d"
+            url: "https://github.com/appareo-systems/ASR/releases/download/0.0.5/eesen_carthage.xcframework.zip",
+            checksum: "b8991b60cb2727c34a1d49405ac46d12009c8a90d0ec635c956ff357fd472875"
         )
     ]
 )
