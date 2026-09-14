c8020cb9c3007852e00d11720dafc33c8e1ed13f
Bill Burgess <bburgess@appareo.com>
2026-09-14 09:39:59 -0500

    Fix target version

--- Package.swift
+++ Package.swift
@@ -13,12 +13,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ASR",
-            url: "https://github.com/appareo-systems/ASR/releases/download/0.0.3/ASR.xcframework.zip",
+            url: "https://github.com/appareo-systems/ASR/releases/download/0.0.4/ASR.xcframework.zip",
             checksum: "4d740627a4eebf14b02790ce9b4893698ef976f269b5e17bb22a41441f322374"
         ),
         .binaryTarget(
             name: "eesen_carthage",
-            url: "https://github.com/appareo-systems/ASR/releases/download/0.0.3/eesen_carthage.xcframework.zip",
+            url: "https://github.com/appareo-systems/ASR/releases/download/0.0.4/eesen_carthage.xcframework.zip",
             checksum: "1a2422cd69cecbea07291ba3aefee42e367efe25795a16192740bd6ba64d688d"
         )
     ]
